43193187ed9c5f710d379e3418d13b73e85271f9
lqk1337 <fredrik.ravndal@gmail.com>
2018-12-02 02:25:25 +0100

    fix

--- default.php
+++ default.php
@@ -10,4 +10,34 @@ $elm->load("profession.xsl");
 $xslt = new XSLTProcessor();
 $xslt->importStylesheet($elm);
 echo $resultat = $xslt->transformToXML($xml);
+
+
+
+$isPressed = false;
+$searchByWord = '';
+$titleList[] = null;
+if(isset($_POST['submit'])) {
+    $searchByWord = $_POST['search'];
+    $professions = $xml->getElementsByTagName('yrker');
+    foreach($professions as $profession) {
+        $professionTitle = $profession->getElementsByTagName('yrkeTittel');
+        if(like_match($professionTitle->item(0)->nodeValue, $searchByWord) ) {
+            $titleList[] = $professionTitle->item(0)->nodeValue;
+        }
+    }
+    print_r($titleList);
+    $isPressed = true;
+}
+
+
+
+
+
+include('search.php');
+
+function like_match($pattern, $subject)
+{
+    $pattern = str_replace('%', '.*', preg_quote($pattern, '/'));
+    return (bool) preg_match("/^{$pattern}$/i", $subject);
+}
 ?>
